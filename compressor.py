0847306bf81a19970d4fe3ed21d21aa8373d720e
Ziwen Wang <ziwen243@outlook.com>
2023-03-12 04:14:08 +0800

    V1.3.0, 对模板的改进以及新功能「统计信息」

--- compressor.py
+++ compressor.py
@@ -14,9 +14,8 @@ def css_minify(css):
     return css
 
 def html_minify(html):
-    # 删除空行
+    # 删除空行、注释和tab
     html = re.sub(r"\n", "", html)
-    # 删除注释
     html = re.sub(r"<!--.*?-->", "", html)
     html = re.sub(r"\t", "", html)
     # 去掉首尾的空格
@@ -25,27 +24,25 @@ def html_minify(html):
 
 def compress():
     # 遍历当前目录下的css目录
-    for css_file in os.listdir("css"):
+    for html_file in os.listdir("html"):
         # 检查是否是.css文件
-        if css_file.endswith(".css"):
-            # 获取对应的html模板名
-            html_template = css_file.replace(".css", ".html")
-            # 检查是否存在对应的html模板
-            if os.path.exists(os.path.join("html", html_template)):
+        if html_file.endswith(".html"):
+            css_file = html_file.replace(".html", ".css")
+            # 读取html模板内容
+            with open(os.path.join("html", html_file), "r", encoding="utf-8") as f:
+                html_content = f.read()
+            if os.path.exists(os.path.join("css", css_file)):
                 # 读取css文件
                 with open(os.path.join("css", css_file), "r") as f:
                     css_content = f.read()
                 # 压缩css文件并获取结果
                 compressed_css = css_minify(css_content)
-                # 读取html模板内容
-                with open(os.path.join("html", html_template), "r") as f:
-                    html_content = f.read()
                 # 替换[compressed-css]为压缩后的文本
-                html_content = html_content.replace("[compressed-css]", compressed_css)
-                # 压缩html文件并获取结果
-                compressed_html = html_minify(html_content)
-                # 写入到templates目录下的对应文件（如果不存在则创建）
-                with open(os.path.join("templates", html_template), "w") as f:
-                    f.write(compressed_html)
+                html_content = html_content.replace("[compressed-css]", "<style>"+compressed_css+"</style>")
+            # 压缩html文件并获取结果
+            compressed_html = html_minify(html_content)
+            # 写入到templates目录下的对应文件（如果不存在则创建）
+            with open(os.path.join("templates", html_file), "w", encoding="utf-8") as f:
+                f.write(compressed_html)
 
 compress()
--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 # -*- coding: utf-8 -*-
 # 导入所需的库
+import asyncio
 from flask import Flask, render_template, abort, request
 from markdown.inlinepatterns import InlineProcessor
-from markdown.blockprocessors import BlockProcessor
 from markdown.extensions import Extension
 import xml.etree.ElementTree as etree
 import os
@@ -11,18 +11,22 @@ import glob
 import markdown
 import re
 import json
+import requests
 from compressor import compress
 
 compress()
 
-fenced_code_pattern = re.compile(r"^(`{3,})(\w*)\n(.*?)\n(`{3,})$", re.MULTILINE | re.DOTALL)
+fenced_code_pattern = re.compile(
+    r"^(`{3,})(\w*)\n(.*?)\n(`{3,})$", re.MULTILINE | re.DOTALL)
 
 # 创建flask应用对象
 app = Flask(__name__)
 
+
 @app.errorhandler(404)
 def page_not_found(e):
-    return render_template('404.html'),404
+    return render_template('404.html'), 404
+
 
 class DelInlineProcessor(InlineProcessor):
     def handleMatch(self, m, data):
@@ -30,10 +34,13 @@ class DelInlineProcessor(InlineProcessor):
         el.text = m.group(1)
         return el, m.start(0), m.end(0)
 
+
 class DelExtension(Extension):
     def extendMarkdown(self, md):
         DEL_PATTERN = r'~~(.*?)~~'
-        md.inlinePatterns.register(DelInlineProcessor(DEL_PATTERN, md), 'del', 175)
+        md.inlinePatterns.register(
+            DelInlineProcessor(DEL_PATTERN, md), 'del', 175)
+
 
 def get_article_title(path):
     # 打开并读取文件内容（只读取第一行）
@@ -64,14 +71,135 @@ def get_file_create_time(path, style="%Y-%m-%d"):
     styled_time = time.strftime(style, time_array)
     return styled_time
 
+
 def replace_fenced_code(match):
-    start = match.group(1) # 开始的反引号
-    code = match.group(3) # 代码内容
-    end = match.group(4) # 结束的反引号
+    start = match.group(1)  # 开始的反引号
+    code = match.group(3)  # 代码内容
+    end = match.group(4)  # 结束的反引号
     # 添加class属性到开始的反引号后面
     new_start = start + " {class=\"fenced_code\"}"
     # 返回替换后的字符串
     return "\n".join([new_start, code, end])
+
+
+def write_log(postname,ip,device):
+    req_time = time.time()
+    try:
+        response = requests.get(f"http://ip-api.com/json/{ip}")
+        country = response.json()["country"]
+    except:
+        country = "Unknown"
+
+    try:
+        # 尝试打开文件
+        f = open("log.json")
+    except FileNotFoundError:
+        # 如果文件不存在，则新建文件并写入空的json对象
+        f = open("log.json", "w")
+        json.dump({}, f)
+    else:
+        # 如果文件存在，则读取json数据
+        try:
+            data = json.load(f)
+        except json.JSONDecodeError:
+            f.close()
+            f = open("log.json", "w")
+            json.dump({}, f)
+            data = {}
+    finally:
+        # 无论是否发生异常，都要关闭文件
+        f.close()
+
+    # 打开json文件，如果不存在则创建一个新的文件
+    with open("log.json", "w") as f:
+        # 检查是否有该postname的键，如果没有则创建一个空列表
+        if postname not in data:
+            data[postname] = []
+        # 将请求记录添加到对应的列表中
+        data[postname].append({"time": req_time, "ip": ip, "device": device, "country": country})
+        # 将更新后的数据写回到文件中
+        json.dump(data, f)
+
+# 定义一个路由，处理/analytics/<postname>的请求，并返回访问量统计页面
+@app.route("/analytics/<path:filepath>")
+def analytics(filepath):
+    # 打开json文件，读取数据，如果不存在则返回错误信息
+    try:
+        with open("log.json", "r") as f:
+            data = json.load(f)
+            logs = data[filepath]
+    except:
+        return "No such post or log file."
+
+    import datetime
+    import collections
+
+    # 获取当前日期
+    today = datetime.date.today()
+
+    # 定义统计变量
+    today_pv = 0  # 今天的访问量
+    week_pv = 0  # 最近7天的访问量
+    month_pv = 0  # 最近30天的访问量
+    total_pv = 0 # 总访问量
+    device_counter = collections.Counter()  # 设备出现次数的计数器
+    browser_counter = collections.Counter()  # 浏览器出现次数的计数器
+    location_counter = collections.Counter()  # 地理位置出现次数的计数器
+    os_counter = collections.Counter() # 操作系统出现次数的计数器
+    today_ip_set = set() # 今天出现过的ip地址集合
+    week_ip_set = set() # 最近7天出现过的ip地址集合
+    month_ip_set = set() # 最近30天出现过的ip地址集合
+    all_ip_set = set() # 所有ip地址集合
+
+    # 遍历json数据中的每一条记录
+    for record in data[filepath]:
+        # 获取记录中的时间戳，ip地址和设备信息
+        time_stamp = record["time"]
+        device_info = record["device"]
+        country = record["country"]
+        ip=record["ip"]
+
+        # 将时间戳转换为日期对象
+        date_obj = datetime.date.fromtimestamp(time_stamp)
+
+        # 判断日期是否在今天，最近7天或最近30天内，并更新相应的访问量变量和ip地址集合变量
+        if date_obj == today:
+            today_pv += 1
+            today_ip_set.add(ip)
+        if (today - date_obj).days < 7:
+            week_pv += 1
+            week_ip_set.add(ip)
+        if (today - date_obj).days < 30:
+            month_pv += 1
+            month_ip_set.add(ip)
+        
+        total_pv+=1
+        all_ip_set.add(ip)
+
+        import user_agents
+        ua=user_agents.parse(device_info)
+        # 解析设备信息中的设备类型和浏览器类型，并更新相应的计数器变量
+        device_type = ua.device.family
+        browser_type = ua.browser.family
+        os_type = ua.os.family
+
+        if ua.is_pc:
+            device_type = "PC"
+
+        location_counter[country] += 1
+        device_counter[device_type] += 1
+        browser_counter[browser_type] += 1
+        os_counter[os_type] += 1
+
+    # 计算独立访客数变量
+    today_uv = len(today_ip_set) # 今天的独立访客数
+    week_uv = len(week_ip_set) # 最近7天的独立访客数
+    month_uv = len(month_ip_set) # 最近30天的独立访客数
+    total_uv = len(all_ip_set)
+    return render_template("analytics.html", main_title=get_article_title("posts/" + filepath + ".md"), filepath=filepath, 
+                           today_pv=today_pv, week_pv=week_pv, month_pv=month_pv, total_pv=total_pv,
+                           today_uv=today_uv, week_uv=week_uv, month_uv=month_uv, total_uv=total_uv,
+                           device_counter=device_counter, browser_counter=browser_counter, location_counter=location_counter, os_counter=os_counter)
 
 @app.route("/<path:filename>")
 def show_post(filename):
@@ -86,12 +214,17 @@ def show_post(filename):
     # 预处理，用于解决围栏代码块的样式问题
     content = fenced_code_pattern.sub(replace_fenced_code, content)
     # 使用markdown模块将内容转换为html
-    html = markdown.markdown(content, extensions=["markdown.extensions.sane_lists", "markdown.extensions.extra", DelExtension()])
+    html = markdown.markdown(content, extensions=[
+                             "markdown.extensions.sane_lists", "markdown.extensions.extra", DelExtension()])
     title = get_article_title(file_path)
     date = get_file_create_time(file_path)
     output = render_template(
-        "article.html", content=html, title=title, date=date, main_title=get_config("title"), links=get_config("links"))
-    # 返回最终的html页面
+        "article.html", content=html, title=title, date=date, filename=filename, main_title=get_config("title"), links=get_config("links"), copyright=get_config("copyright"))
+
+    import multiprocessing
+    log_process=multiprocessing.Process(target=write_log, args=(filename,request.remote_addr,request.user_agent.string,))
+    log_process.start()
+    
     return output
 
 
@@ -127,6 +260,6 @@ def show_home():
 # 运行flask应用
 if __name__ == "__main__":
     # 对于Debug，请使用如下代码
-    # app.run(host="0.0.0.0", port=80, debug=True)
+    #app.run(host="0.0.0.0", port=80, debug=True)
     from waitress import serve
     serve(app, host="127.0.0.1", port=7000)
