ebbb6640b8c545ceef2bcae9fb5f1b65edf5a46e
ZiWen Wang <ziwen243@outlook.com>
2023-12-09 15:19:42 +0800

    [features] 数据库结构和UI改进 添加了文章页面的UI，数据库结构上增添了站点信息`info`表

--- main.py
+++ main.py
@@ -1,20 +1,14 @@
 import sqlite3
-from flask import Flask, render_template, request, redirect, url_for, jsonify
-import markdown,time
-from flask_babel import Babel
+
+import markdown
+import time
+from flask import Flask, render_template, request, jsonify
+
 
 app = Flask(__name__)
-babel = Babel(app)
-
-# 配置Babel
-app.config['BABEL_DEFAULT_LOCALE'] = 'en'  # 默认语言
-app.config['LANGUAGES'] = {
-    'en': 'English',
-    'zh_CN': '中文',
-}
-
 # 数据库文件
 DATABASE = 'db.sqlite'
+
 
 # 辅助函数，用于查询数据库并返回结果
 def query_db(query, args=(), one=False):
@@ -26,10 +20,24 @@ def query_db(query, args=(), one=False):
     conn.close()
     return (rv[0] if rv else None) if one else rv
 
+
+def get_config(key, subkey="value"):
+    query = "SELECT * FROM info WHERE key=?"
+    data = query_db(query, [key], one=True)
+    if data:
+        return data[subkey]
+    else:
+        return None
+
+
+def time_cost(t):
+    return str(round((time.time() - t) * 1000, 3))
+
+
 # 首页路由
 @app.route('/', methods=['GET'])
 def index():
-    t=time.time()
+    t = time.time()
     page = request.args.get('page', 1, type=int)
     per_page = request.args.get('per_page', 10, type=int)
 
@@ -41,27 +49,32 @@ def index():
     page = max(1, min(page, total_pages))
     per_page = max(1, min(per_page, total))
 
-
     # 查询数据库，按modify_time降序排列，获取最新的文章
     query = "SELECT id, title, modify_time, summary FROM articles ORDER BY modify_time DESC LIMIT ? OFFSET ?"
     articles = query_db(query, (per_page, (page - 1) * per_page))
 
-    title="Quill Demo"
+    title = get_config("site_title")
 
-    return render_template('index.html', articles=articles, res_time=str(round((time.time()-t)*1000,3)), title=title, page=page, per_page=per_page, total_pages=total_pages)
+    return render_template('index.html', articles=articles, res_time=time_cost(t), title=title,
+                           page=page, per_page=per_page, total_pages=total_pages)
+
 
 # 文章详情页路由
 @app.route('/article/<int:id>', methods=['GET'])
 def article(id):
+    t = time.time()
     # 查询指定ID的文章内容
-    article = query_db("SELECT content FROM articles WHERE id=?", [id], one=True)
+    article_detail = query_db("SELECT content, title FROM articles WHERE id=?", [id], one=True)
 
-    if article:
+    if article_detail:
         # 将Markdown渲染为HTML
-        html_content = markdown.markdown(article['content'], extensions=['fenced_code'])
-        return render_template('article.html', content=html_content)
+        html_content = markdown.markdown(article_detail['content'], extensions=['pymdownx.extra','markdown_del_ins','pymdownx.superfences','pymdownx.highlight'])
+        return render_template('article.html', content=html_content, title=article_detail['title'],
+                               site_title=get_config("site_title"), homepage=get_config("homepage"),
+                               res_time=time_cost(t), lang=get_config("lang"))
     else:
-        return "Article does not exist."
+        return render_template('404.html'), 404
+
 
 @app.route('/article/', methods=['POST'])
 def create_article():
